ec23f9f6a978bca57ac27ef387c96d236faf82ba
MeghanaVaishnav <meghanavaishnav123@gmail.com>
2021-02-17 12:34:19 +0000

    2nd Commit.

---
+++ GitProject/Pgm1.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Pgm1.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+
+           display 'Hello Wold!'.
+
+       END PROGRAM Pgm1.
